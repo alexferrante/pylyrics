77835ee2db58f85d66958f142e52e125e72c8b2a
alexferrante <alexferrante@github.com>
2019-01-13 16:02:48 -0500

    improving data scraping methods

--- song_scrape.py
+++ song_scrape.py
@@ -20,33 +20,48 @@ def get_top_songs():
         for songs in song_data:
             title = songs.contents[1].text.strip()
             artist = songs.contents[3].text.strip()
-            lyrics = get_lyrics(title, artist)
-            dictionary = {'Title': title, 'Artist': artist, 'Lyrics': lyrics}
+            lyrics = get_lyrics_page(title, artist)
+            f = open('data/text/'+title+'.txt', 'w+')
+            f.write(lyrics)
+            f.close()
+            dictionary = {'Title': title, 'Artist': artist}
             rows_list.append(dictionary)
-        data_frame = pd.DataFrame(rows_list, columns=['Title', 'Artist', 'Lyrics'])
-        data_frame.to_csv('data.csv', encoding='utf-8')
+        data_frame = pd.DataFrame(rows_list, columns=['Title', 'Artist'])
+        data_frame.to_csv('data/data.csv', encoding='utf-8')
     except Exception as e:
         print('Error fetching top songs list:' + e)
 
 #Use the title and artist names to get lyrics for each song from Genius
-def get_lyrics(title, artist):
-    BASE_URL = "http://genius.com/"
+def get_lyrics_page(title, artist):
+    BASE_URL = "http://genius.com"
     #create url using regex 
     build_url = artist + ' ' + title
-    #to do: replace & with 'and' for multiple artists
-    build_url = re.sub('[ ](?=[ ])|[^-_,A-Za-z0-9 ]+', '', build_url)
+    build_url = re.sub(r'[-()\"#&/@;:<>{}`+=~|.!?,]', '', build_url)
+    build_url = build_url.replace('  ', ' ')
     build_url = build_url.replace(' ', '-')
-    build_url = BASE_URL + build_url + '-lyrics'
+    build_url = BASE_URL + '/' + build_url + '-lyrics'
+    #scrape lyrics from web page
     try:
-        #scrape lyrics from web page
-        response = requests.get(build_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
-        soup = BeautifulSoup(response.text, "lxml")
-        lyrics = soup.find('div', class_='lyrics').text.strip()
-        lyrics = re.sub(r'\[[^()]*\]', '', lyrics) #remove brackets and their contents, possibility of using bracketed labels as classifiers
-        return lyrics
+        lyrics = scrape_lyrics(build_url)
     except Exception as e:
-        #to do: implement way to use search bar and check that way (i.e. for long titles / several artists)
-        return "Error fetching page, does not exist"
+        print('Error fetching page, does not exist. Attempting to search manually...') #dealing with javascript-rendered content is more complicated, reproduce AJAX calls and parse JSON 
+        try:
+            build_url = BASE_URL + "/api/search/multi?per_page=5&q=" + title.split(None, 1)[0] + "+" + artist.split(None, 1)[0]
+            response = requests.get(build_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+            soup = BeautifulSoup(response.text, "lxml")
+            json_data = json.loads(soup.find('p').text.strip())
+            lyrics = scrape_lyrics(BASE_URL + json_data["response"]["sections"][0]["hits"][0]["result"]["path"]) 
+        except Exception as e:
+            print('Error fetching lyrics, cannot locate' + e)
+    return lyrics
+
+def scrape_lyrics(url):
+    print(url)
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
+    soup = BeautifulSoup(response.text, "lxml")
+    lyrics = soup.find('div', class_='lyrics').text.strip()
+    lyrics = re.sub(r'\[[^()]*\]', '', lyrics) #remove brackets and their contents, possibility of using bracketed labels as classifiers in the future
+    return lyrics  
 
 #Use the title and artist names to get link to audio 
 def get_song_url(title, artist):
@@ -73,11 +88,15 @@ def get_song_url(title, artist):
                 highest_views = count 
                 highest_href = href
     url = "https://www.youtube.com" + highest_href
-    Downloader().audio_from_youtube(url,
-                                    download=True,
-                                    output_file_path=u'data/audio/'+title+'mp3'
-                                    )
+    try:
+        Downloader().audio_from_youtube(url,
+                                        download=True,
+                                        output_file_path=u'data/audio/'+title+'.mp3'
+                                        preferred_format=u'mp3'
+                                        )
+    except Exception as e:
+        print('Error downloading song audio')
 
+#to do: more informed use of the search bar/ checks to ensure all data is scraped
 #to do: limit rate of requests (time.sleep) to a reasonable level so I dont get banned (import time, put 'time.sleep(requests p second)' in loop)
 #to do: implement map sync via aeneas for forced alignment
-#to do: update dataset accordingly with forced alignment structure (audio corresponds to text)
